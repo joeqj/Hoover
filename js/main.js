dde95daf2deee1aed9ea3d0b3c807b57f9ae10ac
joeqj <heyimjoeqj@gmail.com>
2020-04-05 18:52:40 +0100

    Implemented basic functionality and tone.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 // Based somewhat on this article by Spicy Yoghurt
-// URL for further reading: https://spicyyoghurt.com/tutorials/html5-javascript-game-development/collision-detection-physics
-const app = new PIXI.Application({ backgroundColor: 0x111111 });
-document.body.appendChild(app.view);
+const app = new PIXI.Application({
+	autoResize: true,
+  resolution: devicePixelRatio,
+  backgroundColor: 0x111111
+});
+document.querySelector('#frame').appendChild(app.view);
 
 // Options for how objects interact
-// How fast the red square moves
-const movementSpeed = 0.05;
+// How fast the hoover moves
+const movementSpeed = 0.10;
 
 // Strength of the impulse push between two objects
-const impulsePower = 5;
+const impulsePower = 3;
+
+let vCollision;
+let vCollisionNorm;
 
 // Test For Hit
 // A basic AABB check between two different squares
@@ -17,29 +23,28 @@ function testForAABB(object1, object2) {
     const bounds2 = object2.getBounds();
 
     return bounds1.x < bounds2.x + bounds2.width
-        && bounds1.x + bounds2.width > bounds2.x
+        && bounds1.x + bounds2.width - 38 > bounds2.x
         && bounds1.y < bounds2.y + bounds2.height
-        && bounds1.y + bounds2.height > bounds2.y;
+        && bounds1.y + bounds2.height - 220 > bounds2.y;
 }
 
-// Calculates the results of a collision, allowing us to give an impulse that
-// shoves objects apart
+// Calculates the results of a collision, object2 is hoover
 function collisionResponse(object1, object2) {
     if (!object1 || !object2) {
         return new PIXI.Point(0);
     }
 
-    const vCollision = new PIXI.Point(
+    vCollision = new PIXI.Point(
         object2.x - object1.x,
         object2.y - object1.y,
     );
 
     const distance = Math.sqrt(
         (object2.x - object1.x) * (object2.x - object1.x)
-        + (object2.y - object1.y) * (object2.y - object1.y),
+        + (object2.y - (object1.y)) * (object2.y - object1.y),
     );
 
-    const vCollisionNorm = new PIXI.Point(
+    vCollisionNorm = new PIXI.Point(
         vCollision.x / distance,
         vCollision.y / distance,
     );
@@ -68,64 +73,62 @@ function distanceBetweenTwoPoints(p1, p2) {
     return Math.hypot(a, b);
 }
 
-// The green square we will knock about
-const greenSquare = new PIXI.Sprite(PIXI.Texture.WHITE);
-greenSquare.position.set((app.screen.width - 100) / 2, (app.screen.height - 100) / 2);
-greenSquare.width = 100;
-greenSquare.height = 100;
-greenSquare.tint = '0x00FF00';
-greenSquare.acceleration = new PIXI.Point(0);
-greenSquare.mass = 3;
+// The dust we will knock about
+const dust = new PIXI.Sprite(PIXI.Texture.WHITE);
+dust.position.set((app.screen.width) / 2, (app.screen.height) / 2);
+dust.width = 10;
+dust.height = 10;
+dust.tint = '0xffd500';
+dust.acceleration = new PIXI.Point(0);
+dust.mass = 3;
+dust.alpha = 1;
 
 // The square you move around
-const redSquare = new PIXI.Sprite(PIXI.Texture.WHITE);
-redSquare.position.set(0, 0);
-redSquare.width = 100;
-redSquare.height = 100;
-redSquare.tint = '0xFF0000';
-redSquare.acceleration = new PIXI.Point(0);
-redSquare.mass = 1;
+const hoover = PIXI.Sprite.from('img/hoover.png');
+hoover.position.set(0, 0);
+hoover.width = 50;
+hoover.height = 134;
+hoover.acceleration = new PIXI.Point(0);
+hoover.mass = 0.3;
 
 // Listen for animate update
 app.ticker.add((delta) => {
-    // Applied deacceleration for both squares, done by reducing the
+    // Applied deacceleration for both squares, done by hooverucing the
     // acceleration by 0.01% of the acceleration every loop
-    redSquare.acceleration.set(redSquare.acceleration.x * 0.99, redSquare.acceleration.y * 0.99);
-    greenSquare.acceleration.set(greenSquare.acceleration.x * 0.99, greenSquare.acceleration.y * 0.99);
+    hoover.acceleration.set(hoover.acceleration.x * 0.99, hoover.acceleration.y * 0.99);
+    dust.acceleration.set(dust.acceleration.x * 0.93, dust.acceleration.y * 0.93);
 
     const mouseCoords = app.renderer.plugins.interaction.mouse.global;
 
-    // Check whether the green square ever moves off the screen
-    // If so, reverse acceleration in that direction
-    if (greenSquare.x < 0 || greenSquare.x > (app.screen.width - 100)) {
-        greenSquare.acceleration.x = -greenSquare.acceleration.x;
+    // Check whether the dust ever moves off the screen
+    if (dust.x < 0 || dust.x > (app.screen.width - 20)) {
+        dust.acceleration.x = -dust.acceleration.x;
     }
 
-    if (greenSquare.y < 0 || greenSquare.y > (app.screen.height - 100)) {
-        greenSquare.acceleration.y = -greenSquare.acceleration.y;
+    if (dust.y < 0 || dust.y > (app.screen.height - 20)) {
+        dust.acceleration.y = -dust.acceleration.y;
     }
 
-    // If the green square pops out of the cordon, it pops back into the
-    // middle
-    if ((greenSquare.x < -30 || greenSquare.x > (app.screen.width + 30))
-        || greenSquare.y < -30 || greenSquare.y > (app.screen.height + 30)) {
-        greenSquare.position.set((app.screen.width - 100) / 2, (app.screen.height - 100) / 2);
+    // If the dust pops out of the cordon, it pops back into the middle
+    if ((dust.x < -30 || dust.x > (app.screen.width + 30))
+        || dust.y < -30 || dust.y > (app.screen.height + 30)) {
+        dust.position.set((app.screen.width - 100) / 2, (app.screen.height - 100) / 2);
     }
 
     // If the mouse is off screen, then don't update any further
     if (app.screen.width > mouseCoords.x || mouseCoords.x > 0
         || app.screen.height > mouseCoords.y || mouseCoords.y > 0) {
-        // Get the red square's center point
-        const redSquareCenterPosition = new PIXI.Point(
-            redSquare.x + (redSquare.width * 0.5),
-            redSquare.y + (redSquare.height * 0.5),
+        // Get the hoover's center point
+        const hooverCenterPosition = new PIXI.Point(
+            hoover.x + (hoover.width * 0.5),
+            hoover.y + (hoover.height * 0.5),
         );
 
         // Calculate the direction vector between the mouse pointer and
-        // the red square
+        // the hoover
         const toMouseDirection = new PIXI.Point(
-            mouseCoords.x - redSquareCenterPosition.x,
-            mouseCoords.y - redSquareCenterPosition.y,
+            mouseCoords.x - hooverCenterPosition.x,
+            mouseCoords.y - hooverCenterPosition.y,
         );
 
         // Use the above to figure out the angle that direction has
@@ -135,42 +138,116 @@ app.ticker.add((delta) => {
         );
 
         // Figure out the speed the square should be travelling by, as a
-        // function of how far away from the mouse pointer the red square is
-        const distMouseRedSquare = distanceBetweenTwoPoints(
+        // function of how far away from the mouse pointer the hoover is
+        const distMousehoover = distanceBetweenTwoPoints(
             mouseCoords,
-            redSquareCenterPosition,
+            hooverCenterPosition,
         );
-        const redSpeed = distMouseRedSquare * movementSpeed;
 
-        // Calculate the acceleration of the red square
-        redSquare.acceleration.set(
-            Math.cos(angleToMouse) * redSpeed,
-            Math.sin(angleToMouse) * redSpeed,
+        const hooverSpeed = distMousehoover * movementSpeed;
+
+        // Calculate the acceleration of the hoover
+        hoover.acceleration.set(
+            Math.cos(angleToMouse) * hooverSpeed,
+            Math.sin(angleToMouse) * hooverSpeed,
         );
+
+        if (hooverSpeed > 1 && hooverSpeed < 20) {
+          startHoover();
+        } else {
+          stopHoover();
+        }
     }
 
-    // If the two squares are colliding
-    if (testForAABB(greenSquare, redSquare)) {
-        // Calculate the changes in acceleration that should be made between
-        // each square as a result of the collision
-        const collisionPush = collisionResponse(greenSquare, redSquare);
-        // Set the changes in acceleration for both squares
-        redSquare.acceleration.set(
-            (collisionPush.x * greenSquare.mass),
-            (collisionPush.y * greenSquare.mass),
-        );
-        greenSquare.acceleration.set(
-            -(collisionPush.x * redSquare.mass),
-            -(collisionPush.y * redSquare.mass),
-        );
+    // Colliding
+    if (testForAABB(dust, hoover)) {
+        if (dust.alpha > 0.5) {
+          // Calculate the changes in acceleration that should be made between
+          // each square as a result of the collision
+          let collisionPush = collisionResponse(dust, hoover);
+          // Set the changes in acceleration for both squares
+          hoover.acceleration.set(
+              (collisionPush.x * dust.mass),
+              (collisionPush.y * dust.mass),
+          );
+          dust.acceleration.set(
+              -(collisionPush.x * hoover.mass),
+              -(collisionPush.y * hoover.mass),
+          );
+
+          // dust has been hoovered!
+          dust.alpha = 0.7;
+          setTimeout(function () {
+            dust.alpha = 0;
+            app.stage.removeChild(dust);
+          }, 250);
+        }
     }
 
-    greenSquare.x += greenSquare.acceleration.x * delta;
-    greenSquare.y += greenSquare.acceleration.y * delta;
+    dust.x += dust.acceleration.x * delta / 2;
+    dust.y += dust.acceleration.y * delta;
 
-    redSquare.x += redSquare.acceleration.x * delta;
-    redSquare.y += redSquare.acceleration.y * delta;
+    hoover.x += hoover.acceleration.x * delta;
+    hoover.y += hoover.acceleration.y * delta;
 });
 
 // Add to stage
-app.stage.addChild(redSquare, greenSquare);
+app.stage.addChild(dust, hoover);
+
+// Resize canvas
+window.addEventListener('resize', resize);
+
+function resize() {
+	const parent = app.view.parentNode;
+	app.renderer.resize(parent.clientWidth, parent.clientHeight);
+}
+
+resize();
+
+// Audio
+const synth = new Tone.AMSynth().toMaster();
+synth.set({
+  "harmonicity" : 3,
+  "detune" : 10,
+  "oscillator" : {
+    "type" : "triangle"
+  } ,
+	"envelope" : {
+		"attack" : 0.3,
+    "sustain": 1,
+    "release": 6
+	},
+  "modulation" : {
+    "type" : "sawtooth"
+  } ,
+  "modulationEnvelope" : {
+    "attack" : 0.5 ,
+    "decay" : 0 ,
+    "sustain" : 1 ,
+    "release" : 6
+  }
+});
+
+synth.volume.value = -5;
+
+let started = false;
+
+function startHoover() {
+  //play a middle 'C' for the duration of an 8th note
+  if (started === false) {
+    synth.triggerAttack("D4");
+    started = true;
+  }
+}
+
+function stopHoover() {
+  if (started === true) {
+    synth.triggerRelease();
+    started = false;
+  }
+}
+
+if (Tone.context.state !== 'running') {
+  Tone.context.resume();
+  console.log("yo");
+}
