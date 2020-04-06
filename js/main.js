ad683fcbedc0387660e5a994bcc3daa6846f2176
joeqj <heyimjoeqj@gmail.com>
2020-04-06 22:37:22 +0100

    Added dust as array, improved collision, turned off hoover collision

--- js/main.js
+++ js/main.js
@@ -6,6 +6,17 @@ const app = new PIXI.Application({
 });
 document.querySelector('#frame').appendChild(app.view);
 
+// Resize canvas
+window.addEventListener('resize', resize);
+
+function resize() {
+	const parent = app.view.parentNode;
+	app.renderer.resize(parent.clientWidth, parent.clientHeight);
+}
+
+resize();
+
+
 // Options for how objects interact
 // How fast the hoover moves
 const movementSpeed = 0.10;
@@ -82,14 +93,21 @@ function distanceBetweenTwoPoints(p1, p2) {
 }
 
 // The dust we will knock about
-const dust = new PIXI.Sprite(PIXI.Texture.WHITE);
-dust.position.set((app.screen.width) / 2, (app.screen.height) / 2);
-dust.width = 10;
-dust.height = 10;
-dust.tint = '0xffd500';
-dust.acceleration = new PIXI.Point(0);
-dust.mass = 3;
-dust.alpha = 1;
+let dustArray = [];
+
+for (var i = 0; i < 3000; i++) {
+	var sprite = new PIXI.Sprite(PIXI.Texture.WHITE);
+	sprite.width = 10;
+	sprite.height = 10;
+	sprite.tint = '0x008b8b';
+	sprite.acceleration = new PIXI.Point(0);
+	sprite.mass = 1;
+	sprite.alpha = 1;
+	sprite.name = sprite + i;
+
+	sprite.position.set((Math.floor(Math.random() * app.screen.width)), (Math.floor(Math.random() * app.screen.width)));
+	dustArray.push(sprite);
+}
 
 // The square you move around
 const hoover = PIXI.Sprite.from('assets/hoover.png');
@@ -97,31 +115,40 @@ hoover.position.set(0, 0);
 hoover.width = 50;
 hoover.height = 134;
 hoover.acceleration = new PIXI.Point(0);
-hoover.mass = 0.3;
+hoover.mass = 1;
 
 // Listen for animate update
 app.ticker.add((delta) => {
     // Applied deacceleration for both squares, done by hooverucing the
     // acceleration by 0.01% of the acceleration every loop
     hoover.acceleration.set(hoover.acceleration.x * 0.99, hoover.acceleration.y * 0.99);
-    dust.acceleration.set(dust.acceleration.x * 0.93, dust.acceleration.y * 0.93);
+		for (var i = 0; i < dustArray.length; i++) {
+			dustArray[i].acceleration.set(dustArray[i].acceleration.x * 0.93, dustArray[i].acceleration.y * 0.93);
+		}
 
     const mouseCoords = app.renderer.plugins.interaction.mouse.global;
 
     // Check whether the dust ever moves off the screen
-    if (dust.x < 0 || dust.x > (app.screen.width - 20)) {
-        dust.acceleration.x = -dust.acceleration.x;
-    }
+		for (var i = 0; i < dustArray.length; i++) {
+			if (dustArray[i].x < 0 || dustArray[i].x > (app.screen.width - 20)) {
+	        dustArray[i].acceleration.x = -dustArray[i].acceleration.x;
+	    }
+		}
 
-    if (dust.y < 0 || dust.y > (app.screen.height - 20)) {
-        dust.acceleration.y = -dust.acceleration.y;
-    }
+		for (var i = 0; i < dustArray.length; i++) {
+			if (dustArray[i].y < 0 || dustArray[i].y > (app.screen.height - 20)) {
+	        dustArray[i].acceleration.y = -dustArray[i].acceleration.y;
+	    }
+		}
 
     // If the dust pops out of the cordon, it pops back into the middle
-    if ((dust.x < -30 || dust.x > (app.screen.width + 30))
-        || dust.y < -30 || dust.y > (app.screen.height + 30)) {
-        dust.position.set((app.screen.width - 100) / 2, (app.screen.height - 100) / 2);
-    }
+		for (var i = 0; i < dustArray.length; i++) {
+			if ((dustArray[i].x < -20 || dustArray[i].x > (app.screen.width + 20))
+	        || dustArray[i].y < -20 || dustArray[i].y > (app.screen.height + 20)) {
+	        app.stage.removeChild(dustArray[i]);
+	    }
+		}
+
 
     // If the mouse is off screen, then don't update any further
     if (app.screen.width > mouseCoords.x || mouseCoords.x > 0
@@ -160,7 +187,13 @@ app.ticker.add((delta) => {
             Math.sin(angleToMouse) * hooverSpeed,
         );
 
-        if (mouseDown === true && hooverSpeed > 1 && hooverSpeed < 20) {
+        // if (mouseDown === true && hooverSpeed > 1 && hooverSpeed < 20) {
+        //   startHoover();
+        // } else {
+        //   stopHoover();
+        // }
+
+				if (mouseDown === true) {
           startHoover();
         } else {
           stopHoover();
@@ -168,51 +201,54 @@ app.ticker.add((delta) => {
     }
 
     // Colliding
-    if (testForAABB(dust, hoover)) {
-        if (dust.alpha > 0.5) {
-          // Calculate the changes in acceleration that should be made between
-          // each square as a result of the collision
-          let collisionPush = collisionResponse(dust, hoover);
-          // Set the changes in acceleration for both squares
-          hoover.acceleration.set(
-              (collisionPush.x * dust.mass),
-              (collisionPush.y * dust.mass),
-          );
-          dust.acceleration.set(
-              -(collisionPush.x * hoover.mass),
-              -(collisionPush.y * hoover.mass),
-          );
-
-          // dust has been hoovered!
-          if (mouseDown === true) {
-						dust.alpha = 0.7;
-	          setTimeout(function () {
-	            dust.alpha = 0;
-	            app.stage.removeChild(dust);
-	          }, 250);
-          }
-        }
+    for (var i = 0; i < dustArray.length; i++) {
+			if (testForAABB(dustArray[i], hoover)) {
+	      if (dustArray[i].alpha > 0.5) {
+	        // Calculate the changes in acceleration that should be made between
+	        // each square as a result of the collision
+	        let collisionPush = collisionResponse(dustArray[i], hoover);
+	        // Set the changes in acceleration for dust
+	        dustArray[i].acceleration.set(
+	            (collisionPush.x * hoover.mass / 10),
+	            (collisionPush.y * hoover.mass / 10),
+	        );
+	        hooverDust(dustArray[i], collisionPush);
+	      }
+	    }
     }
 
-    dust.x += dust.acceleration.x * delta / 2;
-    dust.y += dust.acceleration.y * delta;
+		for (var i = 0; i < dustArray.length; i++) {
+			dustArray[i].x += dustArray[i].acceleration.x * delta / 2;
+	    dustArray[i].y += dustArray[i].acceleration.y * delta;
+		}
 
     hoover.x += hoover.acceleration.x * delta;
     hoover.y += hoover.acceleration.y * delta;
 });
 
-// Add to stage
-app.stage.addChild(dust, hoover);
+function hooverDust(dust, collision) {
+	// dust has been hoovered!
+	if (mouseDown === true) {
 
-// Resize canvas
-window.addEventListener('resize', resize);
+		//hoover collision
+		// hoover.acceleration.set(
+		// 		(collision.x * dust.mass / 10),
+		// 		(collision.y * dust.mass / 10),
+		// );
 
-function resize() {
-	const parent = app.view.parentNode;
-	app.renderer.resize(parent.clientWidth, parent.clientHeight);
+		dust.alpha = 0.7;
+		setTimeout(function () {
+			dust.alpha = 0;
+			app.stage.removeChild(dust);
+		}, 250);
+	}
 }
 
-resize();
+// Add to stage
+for (var i = 0; i < dustArray.length; i++) {
+	app.stage.addChild(dustArray[i]);
+}
+app.stage.addChild(hoover);
 
 // Audio
 const mono = new Tone.Mono().toMaster();
